efd2d876588b8f61cafac3dacb97fb2017dc19c3
Fuyuh <aurore.chayrouse@gmail.com>
2020-03-10 21:33:04 +0100

    battleboard icons

--- frontend/src/views/Battleboard.vue
+++ frontend/src/views/Battleboard.vue
@@ -3,9 +3,25 @@
     <h1>Final Order Battles</h1>
     <div class="container" v-for="(battle, index) in battles" :key="index">
       INDEX : {{ index }}
-      {{stringDate(battle.startTime)}}
-       TOTAL FAME : {{ battle.totalFame}}
-        <!-- Page Content goes here -->
+
+    <div class="row">
+        <div class="col s6 ">
+          <span><i class="material-icons ">format_list_numbered</i>ID {{battle.id}}</span>
+        </div>
+        <div class="col s6">
+          <i class="material-icons ">timer</i>DURATION {{battle.startTime}}
+        </div>
+        <div class="col s4">
+          <i class="material-icons ">supervisor_account</i> TOTAL KILL : {{ battle.totalKills}}
+        </div>
+        <div class="col s4">
+          <i class="material-icons ">star</i> TOTAL FAME : {{ battle.totalFame}}
+        </div>
+        <div class="col s4">
+          AVERAGE FAME PER KILL : {{ (battle.totalFame / battle.totalKills).toFixed(0)}}
+        </div>
+    </div>
+    <a class="waves-effect waves-light btn"><i class="material-icons left">search</i>See battleboard</a>
         <table class="striped">
         <thead>
           <tr>
@@ -22,7 +38,7 @@
             <td>{{ guild.name }} ( {{guild.numbers}} )</td>
             <td>{{guild.kills}}/{{guild.deaths}}</td>
             <td>{{(guild.deaths ? guild.kills/guild.deaths : guild.kills)}}</td>
-            <td>{{ guild.killFame }} ({{ (guild.killFame*100)/ battle.totalFame}} %)</td>
+            <td>{{ guild.killFame }} ({{ ((guild.killFame*100)/ battle.totalFame).toFixed(1) }} %)</td>
           </tr>
         </tbody>
       </table>
@@ -37,7 +53,8 @@ export default {
   name: 'battles',
   data: function () {
     return {
-      battles: []
+      battles: [],
+      bestKillFame: { name: '', killfame: 0 }
     }
   },
   computed: {
@@ -82,14 +99,16 @@ export default {
       for (const player in battle.players) {
         guildNumber[battle.players[player].guildId] += 1
       }
-      console.log(guildNumber)
-      console.log(battle.guilds)
-
       for (const guild in battle.guilds) {
         battle.guilds[guild].numbers = guildNumber[guild]
       }
     }
-
+    /* guildBestKillFame: function (guildValues) {
+      if (this.bestKillFame.killfame < guildValues.killFame) {
+        this.bestKillFame.name = guildValues.name
+        this.bestKillFame.killfame = guildValues.killfame
+      }
+    } */
   },
   mounted () {
     this.fetchData()
@@ -97,6 +116,12 @@ export default {
         this.battles = res.data
         this.battles.map(this.missGuild)
         this.battles.map(this.guildsNumber)
+        /* this.battles.forEach(battle => {
+          for (const guild in battle.guilds) {
+            console.log(battle.guilds[guild])
+            battle.bestkdaaa = this.guildBestKillFame(battle.guilds[guild])
+          }
+        }) */
       })
   }
 }
